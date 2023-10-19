980279415d64a7d7b67ab9ac6721c051d51dc3b3
Luis Fernando <luisfernandososnoski@gmail.com>
2023-10-19 01:16:03 -0300

    components dinamicos

--- src/app/components/card-principal/card-principal.component.ts
+++ src/app/components/card-principal/card-principal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-principal',
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-principal.component.css']
 })
 export class CardPrincipalComponent implements OnInit {
-
+  @Input()
+  photo:string=""
+  @Input()
+  cardTitle:string=""
+  @Input()
+  description=""
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/card-secundario/card-secundario.component.ts
+++ src/app/components/card-secundario/card-secundario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-secundario',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-secundario.component.css']
 })
 export class CardSecundarioComponent implements OnInit {
-
+  @Input()
+  Photo:string=""
+  @Input()
+  title:string=""
   constructor() { }
 
   ngOnInit(): void {
